0bbda06b51c3491738d904cf0fd8acc9198a59f6
hyojin530 <dkakfl56@gmail.com>
2020-11-21 13:14:41 +0900

    test

--- product/product.py
+++ product/product.py
@@ -3,18 +3,39 @@ from flask import Blueprint, render_template, request, session, redirect, curren
 from werkzeug.utils import secure_filename
 import datetime
 import os
+from product import product_dao
 
 product_blue = Blueprint('product_blue', __name__)
 
+#메인페이지
 @product_blue.route('/')
 def main_page():
-    html = render_template()    #add html
+    data = product_dao.post_list()
+    html = render_template('', data=data)    #add html
     return 'html'
+
+#검색페이지get
 
 #등록페이지
 
+#상품등록
+
 #상세페이지
+@product_blue.route('/post/post_id=<post_idx>', methods=['get'])
+def post_detail(post_idx):
+    detail = product_dao.post_detail(post_idx)
+    html = render_template('', data=data)
+    return html
 
-#메인페이지
 
-#구독
+#구독페이지
+
+#구독추가
+
+#구독취소
+
+#위시리스트페이지
+
+#위시리스트추가
+
+#위시리스트취소
--- product/product_dao.py
+++ product/product_dao.py
@@ -1,6 +1,29 @@
 from connection.mysql_login import get_connection
 
-def post_list(order):
-    sql = '''select * from post_sell'''
+#order by need, dict
+def post_list():
+    sql = '''select * from posts'''
     
-    return 'OK'
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    finally:
+        if conn is not None: conn.close()
+    
+    return result
+
+#dict
+def post_detail(post_idx):
+    sql = '''select * from posts where post_idx=%s'''
+    
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(sql, post_idx)
+        result = cursor.fetchone()
+    finally:
+        if conn is not None: conn.close()
+    
+    return result
